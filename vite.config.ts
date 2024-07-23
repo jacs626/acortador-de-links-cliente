524a4d344bc8f39ca436e748cee008be9c5af577
Jose Calfuen <josecalfuen@gmail.com>
2024-07-22 20:57:01 -0400

    asdsad

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    // Solo abre el navegador si estamos en desarrollo
+    open: false,
     port: 3000,
   },
 });
